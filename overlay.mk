d98d13e00f6d40b82e654e54d1b3fa12ba4b70c9
Ayus Chatterjee <144832717+ayusc@users.noreply.github.com>
2026-07-25 01:23:33 +0530

    Update overlay.mk

--- overlay.mk
+++ overlay.mk
@@ -75,7 +75,7 @@ PRODUCT_PACKAGES += \
 	treble-overlay-infinix-s4 \
 	treble-overlay-infinix-zero6 \
 	treble-overlay-infinix-zeroxpro \
-    treble-overlay-lava-lavablaze5g \
+	treble-overlay-lava-lavablaze5g \
 	treble-overlay-lenovo-Q706F \
 	treble-overlay-lenovo-Y70 \
 	treble-overlay-lenovo-k5pro \
@@ -453,5 +453,4 @@ PRODUCT_PACKAGES += \
 	treble-overlay-xiaomi-redminote9promax \
 	treble-overlay-xiaomi-redminote9s \
 	treble-overlay-xiaomi-redminote9t \
-	treble-overlay-xiaomi-redmis2 \
-
+	treble-overlay-xiaomi-redmis2
