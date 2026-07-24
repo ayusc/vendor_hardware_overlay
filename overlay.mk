8b06364a4c2270fb5e107db607591e002a7e7ee4
Ayus Chatterjee <144832717+ayusc@users.noreply.github.com>
2026-07-25 01:08:38 +0530

    Update overlay.mk

--- overlay.mk
+++ overlay.mk
@@ -75,6 +75,7 @@ PRODUCT_PACKAGES += \
 	treble-overlay-infinix-s4 \
 	treble-overlay-infinix-zero6 \
 	treble-overlay-infinix-zeroxpro \
+    treble-overlay-lava-lavablaze5g \
 	treble-overlay-lenovo-Q706F \
 	treble-overlay-lenovo-Y70 \
 	treble-overlay-lenovo-k5pro \
